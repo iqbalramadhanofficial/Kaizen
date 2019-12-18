55f0581a5b6bfc004cc6a3263905afff74be56a2
Iqbalofficial <49395900+iqbalramadhanofficial@users.noreply.github.com>
2019-12-18 17:24:28 +0700

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,15 @@
+#Variable adalah Identitasi untuk menampung suatu nilai
+#Menggunakan tanda sama dengan "=" untuk membuat variable
+
+"""" Example """
+
+Harga = 7000
+Benar = True
+Afiliasi = "Universitas Pembangunan Jaya"
+
+""" Output """
+
+print("Harga : " + Harga)
+print("Benar : " + Benar)
+print("Kuliah : " + Afiliasi)
+
